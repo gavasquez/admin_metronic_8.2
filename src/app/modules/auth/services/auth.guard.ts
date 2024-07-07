97edc2d65ff1e6af6d916e767d0a39bd6529ae5e
Gustavo Vasquez <gustavo.vasquez@corhuila.edu.co>
2024-07-07 18:50:38 -0500

    Login

---
+++ src/app/modules/auth/services/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard  {
+  constructor(private authService: AuthService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if(!this.authService.user || this.authService.token){
+      this.authService.logout();
+      return false;
+    }
+    let token = this.authService.token;
+    //* Descodifica una cadena de datos atob y con el split lo cortamos por el punto y obtenemos la primera posicion
+    let expiration = (JSON.parse(atob(token.split(".")[1]))).exp;
+    console.log({expiration});
+    //* Token ha expirado
+    if(Math.floor((new Date().getTime()/1000)) >= expiration){
+      this.authService.logout();
+      return false;
+    }
+    return true;
+  }
+}
